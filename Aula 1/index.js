50b07c66c015294f74ee0073ddb04b53b916b183
Madu0001 <eduarda.a.ferneda@gmail.com>
2025-02-11 10:48:36 -0300

    11/02/25 0.2

--- Aula 1/index.js
+++ Aula 1/index.js
@@ -7,12 +7,12 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send ('Olá, mundo! Servidor rodando com express')
 })
-
-
-
-
-
-
+app.get ('/home', (req, res) => {
+    res.send ('Pagina home')
+})
+app.get ('/login', (req, res) => {
+    res.send ('Pagina Login')
+})
 
 app.listen (port, () => {
     console.log ('Servidor rodando na porta', port)
