347d6a93dec04da1d4e0eb855e306426872cec1d
Madu0001 <169094825+Madu0001@users.noreply.github.com>
2025-03-18 11:01:17 -0300

    Update index.js

--- Aula 3 (18.02)/index.js
+++ Aula 3 (18.02)/index.js
@@ -13,6 +13,7 @@ app.post("/users", (req,res,) =>{
         return res.status(400).json({error: "Todos os campos são obrigatorios"})
     }
     const user = userService.addUser(nome, email, senha, endereço, telefone, cpf);
+    
     res.status(200).json({user});
 
 })
@@ -23,7 +24,30 @@ app.get("/users", (req, res) => {
     res.json(userService.getUsers());
 })
 
+//Rota para excluir usuário pelo ID
+
+app.delete("/users/:id", (req,res) => {
+    const id = parseInt (req.params.id); //Converte o ID para número
+    try {
+        const resultado = userService.deleteUser(id); //Tenta excluir o usuário
+        res.status(200).json(resultado); //Retorna a mensagem de sucesso
+    } catch (erro) {
+        res.status (404).json ({error: erro.message}) //Retorna a mensagem de erro
+    }
+})
+
 const port = 3000;
 app.listen(port, ()=> {
     console.log("servidor rodando na porta: ", port);
+})
+
+app.put("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome, email, senha, endereco, telefone, cpf } = req.body;
+    try{
+        const resultado = userService.alterUser(id, nome, email, senha, endereco, telefone, cpf);
+        res.status(200).json(resultado);
+    }catch(erro){
+        res.status(400).json({error: erro.message});
+    }
 })
